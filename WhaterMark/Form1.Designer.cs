119286dde9a33e5d075c116fe8b99aa9d017bf51
Azizjan Ayupov <litpulla@mail.ru>
2020-07-04 10:06:32 +0600

    Add project files.

---
+++ WhaterMark/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace WhaterMark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chooseImgBtn = new System.Windows.Forms.Button();
+            this.waterMarkTextTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.setBtn = new System.Windows.Forms.Button();
+            this.imageNameLbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fontsCbx = new System.Windows.Forms.ComboBox();
+            this.fontSizeChangeTimer = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.fontPlusBtn = new System.Windows.Forms.Button();
+            this.fontSizeMinusBtn = new System.Windows.Forms.Button();
+            this.horizontalTrBar = new System.Windows.Forms.TrackBar();
+            this.verticalTrBar = new System.Windows.Forms.TrackBar();
+            this.waterMarkLocationLbl = new System.Windows.Forms.Label();
+            this.fontSizeTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horizontalTrBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verticalTrBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(439, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chooseImgBtn
+            // 
+            this.chooseImgBtn.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chooseImgBtn.Location = new System.Drawing.Point(459, 13);
+            this.chooseImgBtn.Name = "chooseImgBtn";
+            this.chooseImgBtn.Size = new System.Drawing.Size(130, 23);
+            this.chooseImgBtn.TabIndex = 1;
+            this.chooseImgBtn.Text = "Choose image";
+            this.chooseImgBtn.UseVisualStyleBackColor = true;
+            this.chooseImgBtn.Click += new System.EventHandler(this.chooseImgBtn_Click);
+            // 
+            // waterMarkTextTxt
+            // 
+            this.waterMarkTextTxt.Location = new System.Drawing.Point(608, 61);
+            this.waterMarkTextTxt.Name = "waterMarkTextTxt";
+            this.waterMarkTextTxt.Size = new System.Drawing.Size(180, 20);
+            this.waterMarkTextTxt.TabIndex = 2;
+            this.waterMarkTextTxt.TextChanged += new System.EventHandler(this.waterMarkTextTxt_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(458, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "WaterMark Text:";
+            // 
+            // setBtn
+            // 
+            this.setBtn.Enabled = false;
+            this.setBtn.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.setBtn.Location = new System.Drawing.Point(521, 339);
+            this.setBtn.Name = "setBtn";
+            this.setBtn.Size = new System.Drawing.Size(75, 23);
+            this.setBtn.TabIndex = 4;
+            this.setBtn.Text = "Set";
+            this.setBtn.UseVisualStyleBackColor = true;
+            this.setBtn.Click += new System.EventHandler(this.setBtn_Click);
+            // 
+            // imageNameLbl
+            // 
+            this.imageNameLbl.AutoSize = true;
+            this.imageNameLbl.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.imageNameLbl.Location = new System.Drawing.Point(596, 22);
+            this.imageNameLbl.Name = "imageNameLbl";
+            this.imageNameLbl.Size = new System.Drawing.Size(0, 15);
+            this.imageNameLbl.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(458, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 14);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "WaterMark font:";
+            // 
+            // fontsCbx
+            // 
+            this.fontsCbx.FormattingEnabled = true;
+            this.fontsCbx.Location = new System.Drawing.Point(608, 98);
+            this.fontsCbx.Name = "fontsCbx";
+            this.fontsCbx.Size = new System.Drawing.Size(180, 21);
+            this.fontsCbx.TabIndex = 7;
+            this.fontsCbx.SelectedIndexChanged += new System.EventHandler(this.fontsCbx_SelectedIndexChanged);
+            // 
+            // fontSizeChangeTimer
+            // 
+            this.fontSizeChangeTimer.Interval = 200;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(458, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 14);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "WaterMark font size:";
+            // 
+            // fontPlusBtn
+            // 
+            this.fontPlusBtn.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fontPlusBtn.Location = new System.Drawing.Point(749, 132);
+            this.fontPlusBtn.Name = "fontPlusBtn";
+            this.fontPlusBtn.Size = new System.Drawing.Size(39, 23);
+            this.fontPlusBtn.TabIndex = 9;
+            this.fontPlusBtn.Text = "+";
+            this.fontPlusBtn.UseVisualStyleBackColor = true;
+            this.fontPlusBtn.Click += new System.EventHandler(this.fontPlusBtn_Click);
+            this.fontPlusBtn.MouseDown += new System.Windows.Forms.MouseEventHandler(this.fontPlusBtn_MouseDown);
+            this.fontPlusBtn.MouseUp += new System.Windows.Forms.MouseEventHandler(this.fontSizeBtn_MouseUp);
+            // 
+            // fontSizeMinusBtn
+            // 
+            this.fontSizeMinusBtn.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fontSizeMinusBtn.Location = new System.Drawing.Point(647, 132);
+            this.fontSizeMinusBtn.Name = "fontSizeMinusBtn";
+            this.fontSizeMinusBtn.Size = new System.Drawing.Size(39, 23);
+            this.fontSizeMinusBtn.TabIndex = 10;
+            this.fontSizeMinusBtn.Text = "-";
+            this.fontSizeMinusBtn.UseVisualStyleBackColor = true;
+            this.fontSizeMinusBtn.Click += new System.EventHandler(this.fontSizeMinusBtn_Click);
+            this.fontSizeMinusBtn.MouseDown += new System.Windows.Forms.MouseEventHandler(this.fontSizeMinusBtn_MouseDown);
+            this.fontSizeMinusBtn.MouseUp += new System.Windows.Forms.MouseEventHandler(this.fontSizeBtn_MouseUp);
+            // 
+            // horizontalTrBar
+            // 
+            this.horizontalTrBar.Location = new System.Drawing.Point(461, 257);
+            this.horizontalTrBar.Maximum = 439;
+            this.horizontalTrBar.Name = "horizontalTrBar";
+            this.horizontalTrBar.Size = new System.Drawing.Size(260, 45);
+            this.horizontalTrBar.TabIndex = 12;
+            this.horizontalTrBar.TickFrequency = 10;
+            this.horizontalTrBar.Scroll += new System.EventHandler(this.horizontalTrBar_Scroll);
+            // 
+            // verticalTrBar
+            // 
+            this.verticalTrBar.Location = new System.Drawing.Point(740, 179);
+            this.verticalTrBar.Maximum = 425;
+            this.verticalTrBar.Name = "verticalTrBar";
+            this.verticalTrBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.verticalTrBar.Size = new System.Drawing.Size(45, 259);
+            this.verticalTrBar.TabIndex = 13;
+            this.verticalTrBar.TickFrequency = 10;
+            this.verticalTrBar.Scroll += new System.EventHandler(this.verticalTrBar_Scroll);
+            // 
+            // waterMarkLocationLbl
+            // 
+            this.waterMarkLocationLbl.AutoSize = true;
+            this.waterMarkLocationLbl.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.waterMarkLocationLbl.Location = new System.Drawing.Point(458, 212);
+            this.waterMarkLocationLbl.Name = "waterMarkLocationLbl";
+            this.waterMarkLocationLbl.Size = new System.Drawing.Size(211, 14);
+            this.waterMarkLocationLbl.TabIndex = 14;
+            this.waterMarkLocationLbl.Text = "WaterMark location: 0, 0";
+            // 
+            // fontSizeTxt
+            // 
+            this.fontSizeTxt.Location = new System.Drawing.Point(692, 132);
+            this.fontSizeTxt.Name = "fontSizeTxt";
+            this.fontSizeTxt.Size = new System.Drawing.Size(51, 20);
+            this.fontSizeTxt.TabIndex = 15;
+            this.fontSizeTxt.Text = "8.0";
+            this.fontSizeTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.fontSizeTxt.TextChanged += new System.EventHandler(this.fontSizeTxt_TextChanged);
+            this.fontSizeTxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.fontSizeTxt_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(456, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 14);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "WaterMark color:";
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.BackColor = System.Drawing.Color.Blue;
+            this.colorBtn.Location = new System.Drawing.Point(614, 174);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(27, 23);
+            this.colorBtn.TabIndex = 17;
+            this.colorBtn.UseVisualStyleBackColor = false;
+            this.colorBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Enabled = false;
+            this.saveBtn.Font = new System.Drawing.Font("Noodle soup", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveBtn.Location = new System.Drawing.Point(623, 339);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 18;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.colorBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.fontSizeTxt);
+            this.Controls.Add(this.waterMarkLocationLbl);
+            this.Controls.Add(this.verticalTrBar);
+            this.Controls.Add(this.horizontalTrBar);
+            this.Controls.Add(this.fontSizeMinusBtn);
+            this.Controls.Add(this.fontPlusBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.fontsCbx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.imageNameLbl);
+            this.Controls.Add(this.setBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.waterMarkTextTxt);
+            this.Controls.Add(this.chooseImgBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WaterMark Setter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.horizontalTrBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.verticalTrBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button chooseImgBtn;
+        private System.Windows.Forms.TextBox waterMarkTextTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button setBtn;
+        private System.Windows.Forms.Label imageNameLbl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox fontsCbx;
+        private System.Windows.Forms.Timer fontSizeChangeTimer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button fontPlusBtn;
+        private System.Windows.Forms.Button fontSizeMinusBtn;
+        private System.Windows.Forms.TrackBar horizontalTrBar;
+        private System.Windows.Forms.TrackBar verticalTrBar;
+        private System.Windows.Forms.Label waterMarkLocationLbl;
+        private System.Windows.Forms.TextBox fontSizeTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button colorBtn;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
+
---
+++ WhaterMark/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhaterMark
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        Font waterMarkFont;
+        Single fontSize = 8.0f;
+        Single fontSizeShift = 0.0f;
+        string filePath;
+        ImageFormat imageFormat;
+        public int MyProperty { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            // Hook up the Elapsed event for the timer. 
+            fontSizeChangeTimer.Tick += new EventHandler(OnTimedEvent);
+            
+            fontsCbx.Items.AddRange(FontFamily.Families.Select(x => x.Name).ToArray());
+            
+        }
+
+        private void chooseImgBtn_Click(object sender, EventArgs e)
+        {
+            // open file dialog   
+            OpenFileDialog open = new OpenFileDialog();
+            // image filters  
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(open.FileName);
+                imageNameLbl.Text = open.SafeFileName;
+                pictureBox1.Image = image;
+                filePath = open.FileName;
+                
+            }
+        }
+
+        private void setBtn_Click(object sender, EventArgs e)
+        {
+            if(image == null)
+            {
+                var dialogResult = MessageBox.Show("Please, choose an image", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                if (dialogResult == DialogResult.OK)
+                    return;
+            }
+            PointF waterMarkLocation = new PointF(horizontalTrBar.Value, verticalTrBar.Value);
+            string waterMarkText = waterMarkTextTxt.Text;
+          
+            waterMarkFont = new Font(fontsCbx.Text, fontSize);
+            
+            using (Graphics graphics = Graphics.FromImage(image))
+            {
+                graphics.DrawString(waterMarkText, waterMarkFont, new SolidBrush(colorBtn.BackColor), waterMarkLocation);
+            }
+            pictureBox1.Image = image;
+            saveBtn.Enabled = true;
+        }
+
+        private void waterMarkTextTxt_TextChanged(object sender, EventArgs e)
+        {
+            if (waterMarkTextTxt.Text != "" && fontsCbx.SelectedIndex > -1)
+            {
+                setBtn.Enabled = true;
+            }
+        }
+
+        private void fontsCbx_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (fontsCbx.SelectedIndex > -1 && waterMarkTextTxt.Text != "")
+            {
+                waterMarkTextTxt.Font = new Font(fontsCbx.Text, 8);
+                setBtn.Enabled = true;
+            }
+        }
+
+        private void fontPlusBtn_MouseDown(object sender, MouseEventArgs e)
+        {
+            fontSizeShift = 0.1f;
+            fontSizeChangeTimer.Enabled = true;
+            fontSizeChangeTimer.Start();
+        }
+
+        private void OnTimedEvent(Object source, EventArgs e)
+        {
+            fontSize += (Single)Math.Round(fontSizeShift, 2);
+            fontSize = (Single)Math.Round(fontSize, 2);
+            fontSizeTxt.Text = fontSize.ToString();
+        }
+            
+        private void fontSizeMinusBtn_MouseDown(object sender, MouseEventArgs e)
+        {
+            fontSizeShift = -0.1f;
+            fontSizeChangeTimer.Enabled = true;
+            fontSizeChangeTimer.Start();
+        }
+
+        private void fontSizeBtn_MouseUp(object sender, MouseEventArgs e)
+        {
+            fontSizeChangeTimer.Enabled = false;
+            fontSizeChangeTimer.Stop();
+        }
+
+        private void fontPlusBtn_Click(object sender, EventArgs e)
+        {
+            fontSize += (Single)Math.Round(0.1f, 2);
+            fontSize = (Single)Math.Round(fontSize, 2);
+            fontSizeTxt.Text = fontSize.ToString();
+        }
+
+        private void fontSizeMinusBtn_Click(object sender, EventArgs e)
+        {
+            fontSize += (Single)Math.Round(-0.1f, 2);
+            fontSize = (Single)Math.Round(fontSize, 2);
+            fontSizeTxt.Text = fontSize.ToString();
+        }
+
+        private void fontSizeTxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+                return;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && (textBox.Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+                return;
+            }
+
+        }
+
+        private void fontSizeTxt_TextChanged(object sender, EventArgs e)
+        {
+            fontSize = (Single)Math.Round(Convert.ToSingle(fontSizeTxt.Text), 2);
+        }
+
+        private void horizontalTrBar_Scroll(object sender, EventArgs e)
+        {
+            waterMarkLocationLbl.Text = "WaterMark location: " + (sender as TrackBar).Value + ", " + verticalTrBar.Value;
+        }
+
+        private void verticalTrBar_Scroll(object sender, EventArgs e)
+        {
+            waterMarkLocationLbl.Text = "WaterMark location: " + horizontalTrBar.Value + ", " + (sender as TrackBar).Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Keeps the user from selecting a custom color.
+            colorDialog1.AllowFullOpen = false;
+            // Allows the user to get help. (The default is false.)
+            colorDialog1.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            colorDialog1.Color = colorBtn.BackColor;
+
+            // Update the text box color if the user clicks OK 
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                colorBtn.BackColor = colorDialog1.Color;
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            image.Save("dfdf.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
